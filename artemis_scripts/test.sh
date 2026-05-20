81c173da8d69a2b5006c44157beed338ef52e3bd
Artemis Git Integration <artemis@turintech.ai>
2026-05-20 09:31:33 +0000

    ``` test: update artemis test script ```

--- artemis_scripts/test.sh
+++ artemis_scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash     
 
 # Import variables
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "${BASH_SOURCE[0]%/*}" >/dev/null 2>&1 && pwd || pwd )"
 source "$DIR/variables.sh"
 
 # Populate TEST with the test command
 TEST="poetry run pytest --benchmark-skip tests/"
 echo "Running test command: $TEST"
-eval $TEST
+exec $TEST
