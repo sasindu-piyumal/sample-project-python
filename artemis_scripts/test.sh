1f53f4d91dd279637f275976f27199ee6b27cd4c
Artemis Git Integration <artemis@turintech.ai>
2026-07-22 09:36:33 +0000

    Run tests without eval

--- artemis_scripts/test.sh
+++ artemis_scripts/test.sh
@@ -4,7 +4,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/variables.sh"
 
-# Populate TEST with the test command
-TEST="poetry run pytest --benchmark-skip tests/"
-echo "Running test command: $TEST"
-eval $TEST
+echo "Running test command: poetry run pytest --benchmark-skip tests/"
+poetry run pytest --benchmark-skip tests/
